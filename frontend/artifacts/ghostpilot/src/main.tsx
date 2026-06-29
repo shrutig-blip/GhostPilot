54af1c7104a8ced7bf1c9015a862898db28c76a3
Shruti Gupta <shrutigupta6002@gmail.com>
2026-06-29 14:23:01 +0530

    Remove incorrect frontend files

--- frontend/artifacts/ghostpilot/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Set dark mode by default
-document.documentElement.classList.add('dark');
-
-createRoot(document.getElementById("root")!).render(<App />);
